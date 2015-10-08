077fd8c7482cc0416c5503221bb622703a0cb2b3
Patrick Livbom <patrick@livbom.se>
2015-10-08 02:15:09 +0200

    released

---
+++ root/App_Plugins/GMaps-old/GMapsValueConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Umbraco.Core.Models.PublishedContent;
+using Umbraco.Core.PropertyEditors;
+
+public class GMapsValueConverter : PropertyValueConverterBase
+{
+    public override bool IsConverter(PublishedPropertyType propertyType)
+    {
+        return "Netaddicts.GMaps".Equals(propertyType.PropertyEditorAlias);
+    }
+
+    public override object ConvertDataToSource(PublishedPropertyType propertyType, object source, bool preview)
+    {
+        if (source == null || string.IsNullOrWhiteSpace(source.ToString())) return null;
+     
+        var coordinates = source.ToString().Split(new[] {","}, StringSplitOptions.RemoveEmptyEntries);
+        var loc = new GMapsLocation();
+        loc.Lat = decimal.Parse(coordinates[0]);
+        loc.Lng = decimal.Parse(coordinates[1]);
+        return loc;
+    }
+}
