57d0c8e0ab0dd477656ca20866ce7317fd78ab64
Patrick Livbom <patrick@livbom.se>
2015-10-08 02:15:09 +0200

    released

--- root/Classes/FB/FeedReader.cs
+++ root/Classes/FB/FeedReader.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.ServiceModel.Syndication;
 using System.Web;
 using System.Xml;
+using System.Xml.Linq;
 
 namespace MarcBachraty.Classes.FB
 {
@@ -46,9 +47,40 @@ namespace MarcBachraty.Classes.FB
             }
             return bannerItems;
         }
+        private DateTime ParseDate(string date)
+        {
+            DateTime result;
+            if (DateTime.TryParse(date, out result))
+                return result;
+            else
+                return DateTime.MinValue;
+        }
 
         public List<BannerItem> YoutubeItems()
         {
+            //try
+            //{
+            //    System.Xml.Linq.XDocument doc = System.Xml.Linq.XDocument.Load(ConfigurationManager.AppSettings["YoutubeFeed"]);
+            //    // Feed/Entry
+            //    var entries = from item in doc.Elements().Where(i => i.Name.LocalName == "entry")
+            //                  select new BannerItem()
+            //                  {
+
+            //                      TypeOfContent = Enum.GetName(typeof(TypeOfContent), TypeOfContent.YoutubeMedia),
+            //    content = new entryContent {Value = item.Elements().First(i => i.Name.LocalName == "content").Value},
+            //    MediaUrl =  item.Elements().First(i => i.Name.LocalName == "thumbnail").Attribute("thumbnail").Value,
+            //                      link =new entryLink {href = item.Elements().First(i => i.Name.LocalName == "link").Attribute("href").Value ,target = "_blank"},
+            //                      published = ParseDate(item.Elements().First(i => i.Name.LocalName == "published").Value).ToString(),
+            //                      title = item.Elements().First(i => i.Name.LocalName == "title").Value
+            //                  };
+            //    return entries.ToList();
+            //}
+            //catch(Exception exception)
+            //{
+            //    throw new Exception(exception.Message);
+            //}
+
+
             Atom10FeedFormatter formatter = new Atom10FeedFormatter();
 
             using (XmlReader reader = XmlReader.Create(ConfigurationManager.AppSettings["YoutubeFeed"]))
@@ -61,27 +93,43 @@ namespace MarcBachraty.Classes.FB
 
             foreach (var e in formatter.Feed.Items)
             {
-                //if (!e.Title.Text.StartsWith("Wallflux"))
-                //{
-                    BannerItem le = new BannerItem();
-                    le.title = e.Title.Text;
-                    le.content = new entryContent
-                    {
-                        Value = ((TextSyndicationContent)e.Content).Text
-                    };
 
-                    le.link = new entryLink
-                    {
-                        href = e.Id,
-                        target = "_blank"
-                    };
-                    le.updated = e.LastUpdatedTime.DateTime.ToShortDateString();
-                    le.published = e.PublishDate.DateTime.ToShortDateString();
-                    le.TypeOfContent = Enum.GetName(typeof(TypeOfContent), TypeOfContent.YoutubeMedia);
-                    bannerItems.Add(le);
+                BannerItem le = new BannerItem();
+                le.title = e.Title.Text;
+                le.link = new entryLink();
+
+
+                le.link.target = "_blank";
+                //foreach (SyndicationElementExtension extension in e.ElementExtensions)
+                //{
+                //    XElement ele = extension..GetObject<xe();
+
+                //    if (ele.ToString().StartsWith("<media:thumbnail"))
+                //    {
+                //        int index = ele.ToString().IndexOf("<media:thumbnail");
+                //        if (index > 0)
+                //        {
+                //            le.MediaUrl = ele.ToString().Substring(0, index).Replace("<media:thumbnail url=\"", "");
+                //            le.MediaUrl.Replace("\" width=\"480\" height=\"360\"/>", "");
+                //        }
+
+
+                //    }
+
                 //}
+
+
+                le.updated = e.LastUpdatedTime.DateTime.ToShortDateString();
+                le.published = e.PublishDate.DateTime.ToShortDateString();
+                le.TypeOfContent = Enum.GetName(typeof(TypeOfContent), TypeOfContent.YoutubeMedia);
+                bannerItems.Add(le);
+
             }
             return bannerItems;
+        }
+        private static T GetExtensionElementValue<T>(SyndicationItem item, string extensionElementName)
+        {
+            return item.ElementExtensions.Where(ee => ee.OuterName == extensionElementName).First().GetObject<T>();
         }
     }
 }
